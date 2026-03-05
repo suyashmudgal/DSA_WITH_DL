599c62a080840a9de59e41a36d206c80419e8f13
Suyash Mudgal <suyashmudgal18@gmail.com>
2026-03-05 23:36:03 +0530

    program 7

--- DSA/Question7.cpp
+++ DSA/Question7.cpp
@@ -1,1 +1,19 @@
 // program 7
+class Solution {
+public:
+    int minOperations(string s) {
+
+        int count1 = 0, count2 = 0;
+
+        for(int i = 0; i < s.size(); i++) {
+
+            if(s[i] != (i % 2 ? '1' : '0'))
+                count1++;
+
+            if(s[i] != (i % 2 ? '0' : '1'))
+                count2++;
+        }
+
+        return min(count1, count2);
+    }
+};
